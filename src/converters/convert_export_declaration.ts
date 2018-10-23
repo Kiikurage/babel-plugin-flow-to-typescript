7c3b8e8b33d6eafe7f123973c0d4d011d9570ca3
nd-02110114 <nd.12021218@gmail.com>
2018-10-23 17:13:00 +0900

    delete unneccesary guard

--- src/converters/convert_export_declaration.ts
+++ src/converters/convert_export_declaration.ts
@@ -4,7 +4,6 @@ import {
 } from '@babel/types';
 
 export function convertEmportDeclaration(path: NodePath<ExportDeclaration>): ExportDeclaration {
-    if ((path.node as any).exportKind === null) return path.node;
     (path.node as any).exportKind = null;
     return path.node;
 }
