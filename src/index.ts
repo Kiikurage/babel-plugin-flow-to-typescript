ea3625d8dd7af294106e13ec2740facab7dbe414
Steel Brain <i@steelbrain.me>
2019-01-22 01:31:00 +0500

    :new: Properly transform nullable type annotations at top level

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {buildPlugin} from './plugin';
 import {ImportDeclaration, ImportSpecifier} from './visitors/import_declaration';
 import {OpaqueType} from './visitors/opaque_type';
-import {TypeAnnotation, TypeAlias} from './visitors/type_annotation';
+import {TypeAnnotation, TypeAlias, NullableTypeAnnotation} from './visitors/type_annotation';
 import {TypeCastExpression} from './visitors/type_cast_expression';
 import {TypeParameterDeclaration} from './visitors/type_parameter_declaration';
 import {ClassMethod, ClassDeclaration} from './visitors/class_declaration';
@@ -10,6 +10,7 @@ import {ExportNamedDeclaration} from './visitors/export_declaration'
 export = buildPlugin([
     TypeAnnotation,
     TypeAlias,
+    NullableTypeAnnotation,
     TypeParameterDeclaration,
     ImportDeclaration,
     ImportSpecifier,
