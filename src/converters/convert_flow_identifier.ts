2358ddee454dd80334e9e0c905850265a5ca4f57
William Swanson <swansontec@gmail.com>
2019-02-08 15:10:31 -0800

    Upgrade to babel v7.0.0 & fix qualified identifiers
    Types like `A.B.C` would crash the plugin before.

---
+++ src/converters/convert_flow_identifier.ts
@@ -0,0 +1,17 @@
+import {
+    Identifier,
+    isIdentifier,
+    QualifiedTypeIdentifier,
+    TSQualifiedName,
+    tsQualifiedName,
+} from '@babel/types';
+
+// A.B.C -> A.B.C
+export function convertFlowIdentifier(
+    id: QualifiedTypeIdentifier | Identifier
+): TSQualifiedName | Identifier {
+    if (isIdentifier(id)) {
+        return id;
+    }
+    return tsQualifiedName(convertFlowIdentifier(id.qualification), id.id);
+}
