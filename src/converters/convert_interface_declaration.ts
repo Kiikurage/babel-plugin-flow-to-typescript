f003e06208f2dec4846c4143bdf648836f202100
Steel Brain <i@steelbrain.me>
2019-06-03 03:19:04 +0500

    2/3: Add support for interface declaration extends and implements

--- src/converters/convert_interface_declaration.ts
+++ src/converters/convert_interface_declaration.ts
@@ -4,9 +4,16 @@ import {
   tsInterfaceBody,
   tsTypeAnnotation,
   tsPropertySignature,
+  tsTypeParameterInstantiation,
+  tsExpressionWithTypeArguments,
+  FlowType,
+  Identifier,
+  TSTypeElement,
+  ClassImplements,
+  InterfaceExtends,
   InterfaceDeclaration,
   TypeParameterDeclaration,
-  TSTypeElement,
+  TSExpressionWithTypeArguments,
 } from '@babel/types';
 import { NodePath } from '@babel/traverse';
 
@@ -15,13 +22,24 @@ import { convertTypeParameterDeclaration } from '../converters/convert_type_para
 
 export function convert_interface_declaration(path: NodePath<InterfaceDeclaration>) {
   const origBody = path.get('body');
+  const origExtends: NodePath<InterfaceExtends>[] = path.get('extends') as any;
+  const origImplements: NodePath<ClassImplements>[] = path.get('implements') as any;
   const origTypeParameters = path.get('typeParameters');
+
+  let origExtendsCombined: Array<NodePath<InterfaceExtends | ClassImplements>> = [];
+  if (Array.isArray(origExtends)) {
+    origExtendsCombined = origExtendsCombined.concat(origExtends);
+  }
+  if (Array.isArray(origImplements)) {
+    origExtendsCombined = origExtendsCombined.concat(origImplements);
+  }
 
   const typeParameters = origTypeParameters.node
     ? convertTypeParameterDeclaration(origTypeParameters as NodePath<TypeParameterDeclaration>)
     : null;
 
   const members: Array<TSTypeElement> = [];
+  const extending: Array<TSExpressionWithTypeArguments> = [];
 
   origBody.node.properties.forEach((property, i) => {
     if (isObjectTypeProperty(property)) {
@@ -35,5 +53,16 @@ export function convert_interface_declaration(path: NodePath<InterfaceDeclaratio
     }
   });
 
-  return tsInterfaceDeclaration(path.node.id, typeParameters, null, tsInterfaceBody(members));
+  origExtendsCombined.forEach(origExtend => {
+    const origExtendTypeParameters: NodePath<FlowType>[] = origExtend
+      .get('typeParameters')
+      .get('params') as any;
+    const parameters = tsTypeParameterInstantiation(
+      origExtendTypeParameters.map(item => convertFlowType(item)),
+    );
+
+    extending.push(tsExpressionWithTypeArguments(origExtend.node.id as Identifier, parameters));
+  });
+
+  return tsInterfaceDeclaration(path.node.id, typeParameters, extending, tsInterfaceBody(members));
 }
--- src/visitors/interface_declaration.ts
+++ src/visitors/interface_declaration.ts
@@ -4,9 +4,5 @@ import { NodePath } from '@babel/traverse';
 import { convert_interface_declaration } from '../converters/convert_interface_declaration';
 
 export function InterfaceDeclaration(path: NodePath<InterfaceDeclaration>) {
-  // extends
-  // body
-  // implements
-  // mixins
   path.replaceWith(convert_interface_declaration(path));
 }
