f28ed006ca883b0954e89a91a7cb6980ae8bcb6c
Steel Brain <i@steelbrain.me>
2019-06-05 10:55:19 +0500

    :art: Split member expression conveter and visitor

---
+++ src/converters/convert_member_expression.ts
@@ -0,0 +1,54 @@
+import {
+  nullLiteral,
+  binaryExpression,
+  memberExpression,
+  conditionalExpression,
+  isOptionalMemberExpression,
+  Identifier,
+  MemberExpression,
+  ConditionalExpression,
+  OptionalMemberExpression,
+} from '@babel/types';
+import { NodePath } from '@babel/traverse';
+
+type MemberType = Identifier | MemberExpression;
+
+function getMemberExpression(members: MemberType[]): MemberExpression | Identifier {
+  if (members.length === 1) {
+    return members[0];
+  }
+
+  const lastMember = members.pop();
+  return memberExpression(
+    members.length === 1 ? members[0] : getMemberExpression(members),
+    lastMember,
+  );
+}
+function getConditionalExpression(members: MemberType[], index: number): ConditionalExpression {
+  const currentMember = getMemberExpression(members.slice(0, index + 1));
+  const isLastMember = index === members.length - 1;
+
+  return conditionalExpression(
+    binaryExpression('==', currentMember, nullLiteral()),
+    nullLiteral(),
+    isLastMember ? currentMember : getConditionalExpression(members, index + 1),
+  );
+}
+
+export function convertMemberExpression(path: NodePath<OptionalMemberExpression>) {
+  const objChainReverse = [];
+  let current: any = path.node;
+
+  do {
+    objChainReverse.push(current.property);
+    if (!isOptionalMemberExpression(current.object)) {
+      objChainReverse.push(current.object);
+      break;
+    }
+    current = current.object;
+  } while (current);
+
+  const objChain = objChainReverse.reverse();
+
+  return getConditionalExpression(objChain, 0);
+}
--- src/visitors/optional_member_expression.ts
+++ src/visitors/optional_member_expression.ts
@@ -1,53 +1,8 @@
-import {
-  nullLiteral,
-  binaryExpression,
-  memberExpression,
-  conditionalExpression,
-  isOptionalMemberExpression,
-  Identifier,
-  MemberExpression,
-  ConditionalExpression,
-  OptionalMemberExpression,
-} from '@babel/types';
+import { OptionalMemberExpression } from '@babel/types';
 import { NodePath } from '@babel/traverse';
 
-type MemberType = Identifier | MemberExpression;
-
-function getMemberExpression(members: MemberType[]): MemberExpression | Identifier {
-  if (members.length === 1) {
-    return members[0];
-  }
-
-  const lastMember = members.pop();
-  return memberExpression(
-    members.length === 1 ? members[0] : getMemberExpression(members),
-    lastMember,
-  );
-}
-function getConditionalExpression(members: MemberType[], index: number): ConditionalExpression {
-  const currentMember = getMemberExpression(members.slice(0, index + 1));
-  const isLastMember = index === members.length - 1;
-
-  return conditionalExpression(
-    binaryExpression('==', currentMember, nullLiteral()),
-    nullLiteral(),
-    isLastMember ? currentMember : getConditionalExpression(members, index + 1),
-  );
-}
+import { convertMemberExpression } from '../converters/convert_member_expression';
 
 export function OptionalMemberExpression(path: NodePath<OptionalMemberExpression>) {
-  const objChainReverse = [];
-  let current: any = path.node;
-
-  do {
-    objChainReverse.push(current.property);
-    if (!isOptionalMemberExpression(current.object)) {
-      objChainReverse.push(current.object);
-      break;
-    }
-    current = current.object;
-  } while (current);
-
-  const objChain = objChainReverse.reverse();
-  path.replaceWith(getConditionalExpression(objChain, 0));
+  path.replaceWith(convertMemberExpression(path));
 }
