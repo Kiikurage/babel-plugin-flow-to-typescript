11b9ea6949b0be20660314438d2e3716c9187b15
Bogdan Savluk <savluk.bogdan@gmail.com>
2019-06-25 00:18:19 +0200

    cleanup convertClassConstructor

--- src/converters/convert_class_constructor.ts
+++ src/converters/convert_class_constructor.ts
@@ -1,10 +1,7 @@
-import { classMethod, ClassMethod } from '@babel/types';
+import { ClassMethod } from '@babel/types';
 
 export function convertClassConstructor(node: ClassMethod): ClassMethod {
   if (node.returnType === null) return node;
 
-  const ret = classMethod(node.kind, node.key, node.params, node.body, node.computed, node.static);
-  ret.returnType = null;
-
-  return ret;
+  return { ...node, returnType: null };
 }
